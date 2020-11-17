d7d11a9436aa79f89ebddedec084e26f28dbb339
Tunc Celik <celiktunc44@gmail.com>
2020-11-17 21:43:12 +0000

    api

---
+++ src/test/java/apitests/simpleGetRequest.java
@@ -0,0 +1,73 @@
+package apitests;
+
+import static io.restassured.RestAssured.*;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class simpleGetRequest {
+    String hrurl = "http://100.25.204.38:1000/ords/hr/regions";
+    @Test
+    public void test1(){
+        Response response = RestAssured.get(hrurl);
+        //print the status code
+        System.out.println(response.statusCode());
+
+        //print the json body
+        response.prettyPrint();
+    }
+    /**
+        Given accept type is json
+        When user sends get request to regions endpoint
+        Then response status code must be 200
+        and body is json format
+     */
+    @Test
+    public void test2(){
+        Response response = RestAssured.given().accept(ContentType.JSON).when().get(hrurl);
+
+        //verify response status code is 200
+        Assert.assertEquals(response.statusCode(),200);
+
+        System.out.println(response.contentType());
+
+        //verify content type is application/json
+        Assert.assertEquals(response.contentType(),"application/json");
+    }
+    //*********YUKARIDAKI ISLEMI API RESTASSURED ILE YAPABILIYORUZ*************
+    @Test
+    public void test3(){
+        RestAssured.given().accept(ContentType.JSON)
+                .when().get(hrurl)
+                .then().assertThat().statusCode(200)
+                .and().contentType("application/json");
+                //daha okunabilir olmasi icin when, then, and'leri koyuyoruz.Zorunlu degil.
+
+    }
+    /**
+        Given accept type is json
+        When user sends get request to regions/2
+        Then response status code must be 200
+        and body is json format
+        and response body contains Americas
+     */
+
+    @Test
+    public void test4(){
+        Response response = given().accept(ContentType.JSON)
+                .when().get(hrurl + "/2");
+
+        //verify status code
+        Assert.assertEquals(response.getStatusCode(),200);
+
+        //verify content type
+        Assert.assertEquals(response.contentType(),"application/json");
+
+        //verify body contains Americas
+        Assert.assertTrue(response.body().asString().contains("Americas"));
+    }
+
+
+}
